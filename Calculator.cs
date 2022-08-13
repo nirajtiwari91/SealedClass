75d8a48a930baa35af418df2e2d72d33299b1065
HP <HP@DESKTOP-LGJ9AI7>
2022-08-13 20:29:47 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+    //sealed class
+    public sealed class Calculator
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator objCal = new Calculator();
+            Console.WriteLine(objCal.Sum(5, 4));
+            Console.ReadLine();
+        }
+    }
+}
